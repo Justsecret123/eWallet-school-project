93cc882b7ce5c6c83a3db2500c8d7a5ccf8c98db
JustSecret123 <ibrahimserouis99@hotmail.com>
2021-10-10 17:26:18 +0100

    UPDATE: Settings --- ADD currencies

--- src/pages/SettingsTab.tsx
+++ src/pages/SettingsTab.tsx
@@ -1,5 +1,13 @@
-import { IonContent, IonHeader, IonItem, IonLabel, IonPage, IonSelect, IonSelectOption, IonTitle, IonToolbar } from "@ionic/react";
+import { IonButton, IonContent, IonHeader, IonIcon, IonInput, IonItem, IonLabel, IonPage, IonSelect, IonSelectOption, IonTitle, IonToolbar } from "@ionic/react";
+import {checkmarkCircle} from "ionicons/icons";
 import "./settings.css";
+import { currency_list } from "../currencies";
+
+const currencies = currency_list;
+
+const compareWith = (o1:any, o2:any) => {
+    return o1 && o2 ? o1.code === o2.code : o1 === o2;
+}
 
 const SettingsTab: React.FC = () => {
     return (
@@ -17,15 +25,26 @@ const SettingsTab: React.FC = () => {
                 </IonHeader>
                 
                 <div className="main-app">
-
+                    <IonItem>
+                        <IonLabel position="stacked" className="labels">Change username</IonLabel>
+                        <IonInput type="text" placeholder="Username..."></IonInput>
+                    </IonItem>
                     <IonItem>
                         <IonLabel position="stacked" className="labels">Change currency</IonLabel>
-                        <IonSelect>
-                            <IonSelectOption>
-
-                            </IonSelectOption>
+                        <IonSelect compareWith={compareWith}>
+                            {
+                                currencies.map(currency => (
+                                    <IonSelectOption key={currency.code} value={currency.code}>
+                                        {currency.code} - {currency.name}
+                                    </IonSelectOption>
+                                ))
+                            }
                         </IonSelect>
                     </IonItem>
+                    <IonButton expand="block" className="custom-button">
+                        Confirm
+                        <IonIcon icon={checkmarkCircle}/>
+                    </IonButton>
 
                 </div>
 
