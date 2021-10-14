c8fc37762c799570f0feaa2f5af6d315edcc0e7f
JustSecret123 <ibrahimserouis99@hotmail.com>
2021-10-14 21:02:57 +0100

    UPDATE: SalaryHist, ExpensesHist ---- ADD: Delete functionalities

--- src/pages/ExpensesHistTab.tsx
+++ src/pages/ExpensesHistTab.tsx
@@ -1,8 +1,9 @@
 import { IonBadge, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent, IonDatetime, IonHeader, IonIcon, IonItemOption, IonItemOptions, IonItemSliding, IonList, IonPage, IonSegment, IonSegmentButton, IonText, IonTitle, IonToolbar } from '@ionic/react';
 import { IonItem, IonLabel } from '@ionic/react';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Database, Storage } from "@ionic/storage";
 import { useHistory } from 'react-router';
+import { trash } from 'ionicons/icons';
 import './expensesHist.css';
 
 
@@ -23,6 +24,8 @@ const ExpensesHistTab: React.FC = () => {
 
     const routerHistory = useHistory();
 
+    const ionList:any = useRef();
+
     useEffect(()=>{
         getExpenseListFromDB();
         getCurrencyFromDB();
@@ -42,12 +45,24 @@ const ExpensesHistTab: React.FC = () => {
     }
 
     const getCurrencyFromDB = async() => {
-        const val = await db.get("currency");
+      const val = await db.get("currency");
         if(val!==null){
             setCurrency(val);
         }else{
             setCurrency("MAD");
         }
+    }
+
+    const removeExpense = (idx:number) => {
+      var newExpenseList = [...expenseList];
+      const index = newExpenseList.indexOf(idx);
+      newExpenseList.splice(idx,1);
+
+      setExpenseList(newExpenseList);
+      db.set("expenses",newExpenseList);
+
+      ionList.current.closeSlidingItems();
+
     }
 
     const setColor = (category:string) => {
@@ -92,13 +107,12 @@ const ExpensesHistTab: React.FC = () => {
         </IonHeader>
 
         <div className="main-app">
-            <IonList mode="ios" lines="none">
+            <IonList mode="ios" lines="none" ref={ionList}>
             {
                 expenseList.map((expense:any, index:any)=>(
                     <IonItemSliding key={index}>
                         <IonItemOptions side="end">
-                            <IonItemOption color="primary" expandable>Edit</IonItemOption>
-                            <IonItemOption color="danger" expandable>Delete</IonItemOption>
+                            <IonItemOption color="danger" onClick={()=>{removeExpense(index)}} expandable>Delete <IonIcon icon={trash}/></IonItemOption>
                         </IonItemOptions>
                         <IonItem>
                           <IonCard mode="ios">
--- src/pages/SalaryHistTab.tsx
+++ src/pages/SalaryHistTab.tsx
@@ -1,8 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Database, Storage } from "@ionic/storage";
 import { useHistory } from 'react-router';
 import './expensesHist.css';
-import { IonContent, IonHeader, IonLabel, IonPage, IonSegment, IonSegmentButton, IonTitle, IonToolbar } from '@ionic/react';
+import './salaryHist.css';
+import { IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonContent, IonHeader, IonIcon, IonItem, IonItemOption, IonItemOptions, IonItemSliding, IonLabel, IonList, IonPage, IonSegment, IonSegmentButton, IonTitle, IonToolbar } from '@ionic/react';
+import { calendar, trash } from 'ionicons/icons';
 
 
 const store = new Storage(); 
@@ -17,13 +19,51 @@ const SalaryHistTab: React.FC = () => {
 
 
     const[db, setDb] = useState<Database | null>(database);
+    const [salaryList, setSalaryList] = useState<any>([]);
+    const [currency, setCurrency] = useState<string>("MAD");
 
     const routerHistory = useHistory();
+
+    const ionList:any = useRef();
+
+    useEffect(()=>{
+        getSalaryHistFromDB();
+        getCurrencyFromDB();
+    });
     
     const redirectToAdd = () => {
         routerHistory.push("/tab1");
     }
 
+    const getSalaryHistFromDB = async() => {
+        const val = await db.get("salaries");
+        if(val!==null){
+            setSalaryList(val);
+        }else{
+            setSalaryList(null);
+        }
+    }
+
+    const getCurrencyFromDB = async() => {
+        const val = await db.get("currency");
+        if(val!==null){
+            setCurrency(val);
+        }else{
+            setCurrency("MAD");
+        }
+    }
+
+    const removeSalary = (idx:number) => {
+        var newSalaryList = [...salaryList];
+        const index = newSalaryList.indexOf(idx);
+        newSalaryList.splice(idx,1);
+
+        setSalaryList(newSalaryList);
+        db.set("salaries",newSalaryList);
+
+        ionList.current.closeSlidingItems();
+        
+    }
 
     return(
         <IonPage>
@@ -47,6 +87,31 @@ const SalaryHistTab: React.FC = () => {
                     <IonTitle size="large">Salary: history</IonTitle>
                 </IonToolbar>
             </IonHeader>
+            <div className="main-app">
+                <IonList mode="ios" lines="none" ref={ionList}>
+                    {
+                        salaryList.map((salary:any, index:any)=>(
+                            <IonItemSliding>
+                                <IonItemOptions side="end">
+                                    <IonItemOption color="danger" expandable onClick={()=>{removeSalary(index)}}>Delete <IonIcon icon={trash}/> </IonItemOption>                                  
+                                </IonItemOptions>
+                                <IonItem>
+                                    <IonCard mode="ios">
+                                        <IonCardHeader>
+                                            <IonCardTitle>
+                                                <IonIcon icon={calendar}/>
+                                                Payday: {salary.date}</IonCardTitle>
+                                        </IonCardHeader>
+                                        <IonCardContent>
+                                            Amount : {salary.salary} {currency}
+                                        </IonCardContent>
+                                    </IonCard>
+                                </IonItem>
+                            </IonItemSliding>
+                        ))
+                    }
+                </IonList>
+            </div>
       </IonContent>
     </IonPage>
     )
