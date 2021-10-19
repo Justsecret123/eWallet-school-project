95e0392c63edabf18c6d7affb1b2d361adef5791
JustSecret123 <ibrahimserouis99@hotmail.com>
2021-10-19 02:06:16 +0100

    UPDATE Charts --- ADD: Salary by month

--- src/pages/ChartsTab.tsx
+++ src/pages/ChartsTab.tsx
@@ -12,13 +12,43 @@ store.create().then(function(result){
   database = result;
 });
 
-const colors:any = {
+const categoryColors:any = {
   "Housing": "#3dc2ff",
   "Food": "#3dc2ff", 
   "Bills": "#2dd36f", 
   "Clothes": "#ffc409",
   "Extra": "#eb445a"
 }
+
+const monthColors:any = {
+  "0": "#3dc2ff",
+  "1": "#3dc2ff", 
+  "2": "#2dd36f", 
+  "3": "#ffc409",
+  "4": "#eb445a", 
+  "5": "#92949c", 
+  "6": "#f4f5f8", 
+  "7": "#5260ff", 
+  "8": "#3dc2ff",
+  "9": "#3dc2ff",
+  "10": "#2dd36f",
+  "11": "#ffc409"
+}
+
+const monthNames:any = {
+  "0": "Jan",
+  "1": "Fev", 
+  "2": "Mar", 
+  "3": "Apr",
+  "4": "May", 
+  "5": "Jun", 
+  "6": "Jul", 
+  "7": "Aug", 
+  "8": "Sep",
+  "9": "Oct",
+  "10": "Nov",
+  "11": "Dec"
+};
 
 const initialTotals:{} = {
   "Housing":0, 
@@ -49,6 +79,37 @@ const categoryOptions:{} = {
     }
   }, 
   backgroundColor: "transparent", 
+  chartArea: {
+    textStyle: {
+      color: "white"
+    }
+  }, 
+  legend: {position: "none"}
+};
+
+const monthOptions:{} = {
+  title: "",
+  titleTextStyle: {
+    color: "white"
+  },
+  hAxis: {
+    slantedText:true,
+    slantedTextAngle:45,
+    title: "Month", 
+    titleTextStyle: {
+      color: "white"
+    },
+    textStyle: {
+      color: "white"
+    }
+  }, 
+  vAxis: {
+    minValue: 0, 
+    textStyle: {
+      color: "white"
+    }
+  }, 
+  backgroundColor: "transparent", 
   charArea: {
     textStyle: {
       color: "white"
@@ -57,22 +118,72 @@ const categoryOptions:{} = {
   legend: {position: "none"}
 };
 
+const salaryOptions:{} = {
+  title: "",
+  colors: ["#3dc2ff"],
+  titleTextStyle: {
+    color: "white"
+  },
+  hAxis: {
+    slantedText:true,
+    slantedTextAngle:45,
+    title: "Month", 
+    titleTextStyle: {
+      color: "white"
+    },
+    textStyle: {
+      color: "white"
+    }, 
+    gridlines: {
+      interval: 0,
+      color: "none"
+    }
+  }, 
+  vAxis: {
+    minValue: 0, 
+    textStyle: {
+      color: "white"
+    }, 
+    gridlines: {
+      interval: 0,
+      color: "none"
+    }
+  }, 
+  backgroundColor: "transparent", 
+  chartArea: {
+    textStyle: {
+      color: "white"
+    }
+  }, 
+  annotations: {
+    textStyle: {
+      color: "white"
+    }
+  },
+  legend: {position: "none"}
+};
+
 const ChartsTab: React.FC = () => {
   
   const [db, setDb] = useState<Database | null>(database);
   
   const [expenseList, setExpenseList] = useState<any>([]);
+  const [salaryList, setSalaryList] = useState<any>([]);
   const [totalsByCategory, setTotalsByCategory] = useState<{}>(initialTotals);
   const [categoryCharts, setCategoryCharts] = useState<any>([]);
+  const [monthCharts, setMonthCharts] = useState<any>([]);
+  const [salaryCharts, setSalaryCharts] = useState<any>([]);
 
   const [trigger, setTrigger] = useState<boolean>(true);
 
   useEffect(()=>{
     getExpenseListFromDB();
+    getSalaryListFromDB();
   },[trigger]);
 
   useIonViewDidEnter(()=>{
     getExpenseListFromDB();
+    getSalaryListFromDB();
   })
 
   const getExpenseListFromDB = async() => {
@@ -81,6 +192,14 @@ const ChartsTab: React.FC = () => {
       setExpenseList(val);
       getTotalExpensesByCategory(val);
       getTotalExpensesByMonth(val);
+    }
+  };
+
+  const getSalaryListFromDB = async() => {
+    const val = await db.get("salaries");
+    if(val!==null){
+      setSalaryList(val);
+      getTotalSalaryByMonth(val);
     }
   };
 
@@ -99,20 +218,36 @@ const ChartsTab: React.FC = () => {
     setTotalsByCategory(newTotals);
     let data:any = [["Category","Amount", {role: "style"}, {role: "annotation"}]];
     for (const [key, value] of Object.entries(newTotals)) {
-      data.push([key,value, colors[key], value]);
+      data.push([key,value, categoryColors[key], value]);
     }
-    console.log(data);
     setCategoryCharts(data);
   };
 
   const getTotalExpensesByMonth = (expenses:any) => {
     const newTotals:any = {"0":0, "1":0, "2":0, "3":0, "4":0, "5": 0, "6": 0, "7": 0, "8": 0, "9": 0, "10": 0, "11": 0};
-    expenses.map((expense:any)=>{
-      let date:Date = new Date(expense.date);
-      console.log("Month: ", date.getMonth());
+    expenses.map((expense:any, index:number)=>{
+      let month:string = new Date(expense.date).getMonth().toString();
+      newTotals[month] += expense.amount;
     });
+    let data:any = [["Month","Amount", {role: "style"}, {role: "annotation"}]];
+    for (const [key, value] of Object.entries(newTotals)) {
+      data.push([monthNames[key],value, monthColors[key], value]);
+    }
+    setMonthCharts(data);
   }
 
+  const getTotalSalaryByMonth = (salaries:any) => {
+    const newTotals:any = {"0":0, "1":0, "2":0, "3":0, "4":0, "5": 0, "6": 0, "7": 0, "8": 0, "9": 0, "10": 0, "11": 0};
+    salaries.map((salary:any)=>{
+      let month:string = new Date(salary.date).getMonth().toString();
+      newTotals[month] += salary.salary;
+    });
+    let data:any = [["Month","Amount", {role: "annotation"}]];
+    for (const [key, value] of Object.entries(newTotals)) {
+      data.push([monthNames[key],value,value]);
+    }
+    setSalaryCharts(data);
+  }
 
 
   return (
@@ -129,18 +264,41 @@ const ChartsTab: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <div className="main-app">
-        <h1>Expenses by category</h1>
-          <IonItem>
+          <h1>Expenses by category</h1>
+          <IonItem lines="none">
             <Chart
               width={"100%"}
-              height={300}
+              height={250}
               chartType="BarChart"
               loader={<div>Loading Chart</div>}
               data = {categoryCharts}
               options = {categoryOptions}
-
             >
             </Chart>
+          </IonItem >
+          <h1>Expenses by month</h1>
+          <IonItem lines="none">
+            <Chart
+                width={"100%"}
+                height={280}
+                chartType="ColumnChart"
+                loader={<div>Loading Chart</div>}
+                data = {monthCharts}
+                options = {monthOptions}
+              >
+              </Chart>
+          </IonItem>
+          <h1> Salary history by month </h1>
+          <IonItem lines="none">
+            <Chart
+                  width={"100%"}
+                  height={200}
+                  chartType="LineChart"
+                  loader={<div>Loading Chart</div>}
+                  data = {salaryCharts}
+                  options = {salaryOptions}
+                >
+              </Chart>            
           </IonItem>
         </div>
       </IonContent>
